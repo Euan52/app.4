b0b5138c978de89edd14cf38220f551ae1a72b06
Euan52 <a0938769037@gmail.com>
2026-03-26 17:32:21 +0800

    Update untitled1 (1).py

--- untitled1 (1).py
+++ untitled1 (1).py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1RGc6CSJ-MwpMEWOyR85l9fcxwDOkbxac
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import re
